76485f9a91efabe5103fd7bf3146c478d3b2e8da
Amir <amir.almohamed@gmail.com>
2022-08-13 17:36:36 +0400

    Add init flutter project to main branch

--- lib/src/network/aad_oauth_client.dart
+++ lib/src/network/aad_oauth_client.dart
@@ -8,21 +8,22 @@ class AadOauthClient{
 
   static final Config config = Config(
     //YOUR_TENANT_ID
-    tenant: '',
+    tenant: '56cfddef-d879-492c-bbe9-cec22053fc78',
 
     //YOUR_CLIENT_ID
-    clientId: '',
+    clientId: 'e263e3da-ab88-41f9-a5b3-5f99865ea29e',
 
     //YOUR_DYNAMIC_CRM_API
-    scope: 'https://exapmle.api.crm4.dynamics.com/user_impersonation',
+    scope: 'https://org34320f4f.api.crm4.dynamics.com/user_impersonation',
 
     redirectUri: 'https://login.live.com/oauth20_desktop.srf',
 
     //YOUR_CLIENT_SECRET
-    //clientSecret: '',
+    //clientSecret: '22m8Q~ASZh8pfshj913q9_F04AXr.z.sKsMb6bxB',
 
     //navigatorKey: getIt<AadOauthClient>().navigatorKey,
     //isB2C: true,
+
   );
 
   final AadOAuth oauth = AadOAuth(config);
